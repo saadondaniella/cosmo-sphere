c04b6fd132f67b20e883a1b41060fef29554e774
Daniella Saadon <saadondaniella@gmail.com>
2025-11-04 23:14:36 +0100

    Första filerna, uppstart av projektet

---
+++ footer.php
@@ -0,0 +1,48 @@
+<section class="footer-bar">
+
+    <div class="footer-list">
+        <ul>
+            <li class="title-footer"> Follow us</li><br>
+            <li><a href="https://www.facebook.com/">Facebook</a></li>
+            <li><a href="https://www.tiktok.com/">TikTok</a></li>
+            <li><a href="https://www.instagram.com">Instagram</a></li>
+            <li><a href="https://se.linkedin.com/">Linkedin</a></li>
+        </ul>
+    </div>
+    <div class="footer-list">
+        <ul>
+            <li class="title-footer">General information</li><br>
+            <li>Adress: Kungsgatan 40
+                414 55 Göteborg</li>
+            <li>Phone: +46 745-0033</li>
+            <li>email: cosmo.sphere@cinema.se</li>
+            <li>Costum service</li>
+        </ul>
+    </div>
+    <div class="footer-list">
+        <ul>
+            <li class="title-footer">Resources</li><br>
+
+            <li> FAQs</li>
+            <li>Newsletter Signup</li>
+        </ul>
+    </div>
+
+    <div class="footer-list">
+        <ul>
+            <li class="title-footer"> About us </li><br>
+            <li>About</li>
+            <li>Contact us</li>
+            <li> Studio</li>
+            <li>Press</li>
+            <li>Careers</li>
+            <li>Investors</li>
+        </ul>
+    </div>
+</section>
+
+<img src="/pictures/WALL_Elila.png" alt="WALL_E" class="moving-image">
+
+</body>
+
+</html>
