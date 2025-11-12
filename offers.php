aaa04e1c527dd8003afdbb948b86a929fafeebd2
AllanTran90 <allan_sweden90@hotmail.com>
2025-11-12 11:53:23 +0100

    update offers.php & offers.css

---
+++ offers.php
@@ -0,0 +1,31 @@
+<?php 
+require __DIR__ . "/header.php";
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Offers</title>
+</head>
+<body>
+
+    <div class="container">
+    </div>  
+    <img src="/pictures/b0007ea8-1b92-4314-be11-19bb71de5393.jpeg" alt="pink planet">
+
+    <div class="rectangle">Popcorn n Soda
+    </div>
+    <img src="/pictures/download.jpeg" alt="popcorn">
+
+    <div class="rectangle">Chips n Soda
+    </div>
+    <img src="/pictures/enhanced-buzz-32759-1375096393-12.webp" alt="chips">
+
+    <div class="ff"> Free flow</div>  
+        
+    <img src="" alt="">
+
+</body>
+</html>
