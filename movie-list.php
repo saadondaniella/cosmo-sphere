e597d0280625a767d4357a7cadffa536375dce48
Robin Andersson <danrobin.andersson@outlook.com>
2025-11-16 16:11:44 +0100

    Added more movies and desktop styling

--- movie-list.php
+++ movie-list.php
@@ -1,4 +1,96 @@
 <div class="movies-list-container">
+
+    <div class="movie-item Predator">
+        <h2 class="movie-title">Predator Badlands</h2>
+        <img class="movie-img" src="pictures/Preadator badlands.jpg" alt="">
+        <div class="buttons-container">
+            <div class="movie-buttons-blue">
+                <button class="movie-button">
+                    <img src="pictures/ticket-perforated.svg" alt="">
+                </button>
+                <button class="movie-button">
+                    <img src="pictures/film.svg" alt="">
+                </button>
+            </div>
+        </div>
+    </div>
+
+    <div class="movie-item Wall-E">
+        <h2 class="movie-title">Wall E</h2>
+        <img class="movie-img" src="pictures/Wall-E2.jpg" alt="">
+        <div class="buttons-container">
+            <div class="movie-buttons-blue">
+                <button class="movie-button">
+                    <img src="pictures/ticket-perforated.svg" alt="">
+                </button>
+                <button class="movie-button">
+                    <img src="pictures/film.svg" alt="">
+                </button>
+            </div>
+        </div>
+    </div>
+
+    <div class="movie-item District 9">
+        <h2 class="movie-title">District 9</h2>
+        <img class="movie-img" src="pictures/District 9.jpg" alt="">
+        <div class="buttons-container">
+            <div class="movie-buttons-blue">
+                <button class="movie-button">
+                    <img src="pictures/ticket-perforated.svg" alt="">
+                </button>
+                <button class="movie-button">
+                    <img src="pictures/film.svg" alt="">
+                </button>
+            </div>
+        </div>
+    </div>
+
+    <div class="movie-item Intestellar">
+        <h2 class="movie-title">Interstellar</h2>
+        <img class="movie-img" src="pictures/Interstellar.jpg" alt="">
+        <div class="buttons-container">
+            <div class="movie-buttons-pink">
+                <button class="movie-button">
+                    <img src="pictures/ticket-perforated.svg" alt="">
+                </button>
+                <button class="movie-button">
+                    <img src="pictures/film.svg" alt="">
+                </button>
+            </div>
+        </div>
+    </div>
+
+    <div class="movie-item Inception">
+        <h2 class="movie-title">Inception</h2>
+        <img class="movie-img" src="pictures/Inception.webp" alt="">
+        <div class="buttons-container">
+            <div class="movie-buttons-pink">
+                <button class="movie-button">
+                    <img src="pictures/ticket-perforated.svg" alt="">
+                </button>
+                <button class="movie-button">
+                    <img src="pictures/film.svg" alt="">
+                </button>
+            </div>
+        </div>
+    </div>
+
+    <div class="movie-item Dune">
+        <h2 class="movie-title">Dune</h2>
+        <img class="movie-img" src="pictures/Dune.jpg" alt="">
+        <div class="buttons-container">
+            <div class="movie-buttons-pink">
+                <button class="movie-button">
+                    <img src="pictures/ticket-perforated.svg" alt="">
+                </button>
+                <button class="movie-button">
+                    <img src="pictures/film.svg" alt="">
+                </button>
+            </div>
+        </div>
+    </div>
+
+
     <div class="movie-item future-1">
         <h2 class="movie-title">Back to the future</h2>
         <img class="movie-img" src="pictures/Back to the future 1.jpg" alt="">
@@ -59,7 +151,7 @@
     </div>
 
     <div class="movie-item terminator-2">
-        <h2 class="movie-title">Terminator</h2>
+        <h2 class="movie-title">Terminator 2</h2>
         <img class="movie-img" src="pictures/Terminator 2.jpg" alt="">
         <div class="buttons-container"></div>
         <div class="movie-buttons-pink">
@@ -73,7 +165,7 @@
     </div>
 
     <div class="movie-item terminator-3">
-        <h2 class="movie-title">Terminator</h2>
+        <h2 class="movie-title">Terminator 3</h2>
         <img class="movie-img" src="pictures/Terminator 3.jpg" alt="">
         <div class="buttons-container"></div>
         <div class="movie-buttons-pink">
@@ -85,4 +177,48 @@
             </button>
         </div>
     </div>
+
+    <div class="movie-item Jurassic-Park">
+        <h2 class="movie-title">Jurassic Park</h2>
+        <img class="movie-img" src="pictures/Jurassic Park.jpg" alt="">
+        <div class="buttons-container"></div>
+        <div class="movie-buttons-pink">
+            <button class="movie-button">
+                <img src="pictures/ticket-perforated.svg" alt="">
+            </button>
+            <button class="movie-button">
+                <img src="pictures/film.svg" alt="">
+            </button>
+        </div>
+    </div>
+
+    <div class="movie-item Jurassic-Park 2">
+        <h2 class="movie-title">Jurassic Park 2</h2>
+        <img class="movie-img" src="pictures/Jurassic Park 2.jpg" alt="">
+        <div class="buttons-container"></div>
+        <div class="movie-buttons-pink">
+            <button class="movie-button">
+                <img src="pictures/ticket-perforated.svg" alt="">
+            </button>
+            <button class="movie-button">
+                <img src="pictures/film.svg" alt="">
+            </button>
+        </div>
+    </div>
+
+
+    <div class="movie-item Jurassic-Park 3">
+        <h2 class="movie-title">Jurassic Park 3</h2>
+        <img class="movie-img" src="pictures/Jurassic Park 3.jpg" alt="">
+        <div class="buttons-container"></div>
+        <div class="movie-buttons-pink">
+            <button class="movie-button">
+                <img src="pictures/ticket-perforated.svg" alt="">
+            </button>
+            <button class="movie-button">
+                <img src="pictures/film.svg" alt="">
+            </button>
+        </div>
+    </div>
+
 </div>
