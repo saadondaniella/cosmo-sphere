3620aed1943d6e52cd4208c318fce991a6a88a6c
AllanTran90 <allan_sweden90@hotmail.com>
2025-11-30 16:33:42 +0100

    gjorde hb meny samt fixade hover över offers

--- Header.php
+++ Header.php
@@ -15,18 +15,40 @@
 </head>
 
 <body class="home">
-    <header class="mobile-header">
-        <a href="movie.php"> <img class="mobile-image" src="/pictures/Sidebar.png" alt="Liten menyikon"></a>
-    </header>
+    
+<!DOCTYPE html>
+<html lang="en">
 
-    <header class="site-header">
-        <nav class="navbar" aria-label="Main">
-            <ul class="navlist">
-                <li><a href="/movie.php">Movies</a></li>
-                <li><a href="/index.php">Tickets</a></li>
-                <li><a href="/index.php">Premium</a></li>
-                <li><a href="/index.php">Offers</a></li>
-                <li><a href="/index.php">Shop</a></li>
-            </ul>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cosmo Sphere Cinema</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Michroma&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="movie-list.css">
+    <link rel="stylesheet" href="mediaquary.css">
+    <link rel="stylesheet" href="offers.css">
+</head>
+
+<body class="home">
+    <header>
+        <!-- Hamburger checkbox -->
+        <input type="checkbox" id="menu-toggle">
+
+        <!-- Hamburger ikon -->
+        <label for="menu-toggle" class="menu-icon">
+          <div class="bar"></div>
+          <div class="bar"></div>
+          <div class="bar"></div>
+        </label>
+
+        <!-- Navigering -->
+        <nav class="nav">
+          <div class="nav-links">
+              <a href="index">HOME</a>
+              <a href="movie.php">MOVIE</a>           
+          </div>
         </nav>
     </header>
